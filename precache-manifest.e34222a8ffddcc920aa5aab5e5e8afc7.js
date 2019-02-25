3f5854bd2cf51afe15457b4913c019f02a09c453
scwebd <scwebd@gmail.com>
2019-02-25 16:44:58 -0700

    Updates

---
+++ precache-manifest.e34222a8ffddcc920aa5aab5e5e8afc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8ce16c5fdd524254b4b",
+    "url": "/clicky-game/static/css/main.717533d9.chunk.css"
+  },
+  {
+    "revision": "b8ce16c5fdd524254b4b",
+    "url": "/clicky-game/static/js/main.b8ce16c5.chunk.js"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "fb5790871afa84eb98dc",
+    "url": "/clicky-game/static/js/2.fb579087.chunk.js"
+  },
+  {
+    "revision": "1023b5aaa2cc21494624d6dc5a55e4b6",
+    "url": "/clicky-game/index.html"
+  }
+];
